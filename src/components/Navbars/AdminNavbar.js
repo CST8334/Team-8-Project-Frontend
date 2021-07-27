c7b78c9df161ce426af0df4a4b50ee447b68e4b1
mich0216 <mich0216@algonquinlive.com>
2021-07-27 17:40:37 -0400

    initial commit

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -16,12 +16,20 @@
 
 */
 import React, { Component } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation,useHistory } from "react-router-dom";
 import { Navbar, Container, Nav, Dropdown, Button } from "react-bootstrap";
 
 import routes from "routes.js";
 
 function Header() {
+    const history = useHistory();
+    function Logout()
+    {
+    console.clear();
+    history.push("/login");
+
+    }
+
   const location = useLocation();
   const mobileSidebarToggle = (e) => {
     e.preventDefault();
@@ -198,7 +206,7 @@ function Header() {
                 href="#pablo"
                 onClick={(e) => e.preventDefault()}
               >
-                <span className="no-icon">Log out</span>
+                <span className="no-icon" onClick={Logout}>Log out</span>
               </Nav.Link>
             </Nav.Item>
           </Nav>
